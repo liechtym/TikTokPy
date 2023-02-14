c13ff76d9488dcd59734967dbe3c2bc956e83510
Matthew Liechty <matthew@liechty.org>
2023-02-14 10:09:08 -0500

    Update async_api.py

--- src/tiktokapipy/async_api.py
+++ src/tiktokapipy/async_api.py
@@ -176,7 +176,8 @@ class AsyncTikTokAPI(TikTokAPI):
                 res = await page.goto(link)
                 headers = await res.all_headers()
                 print('Headers:', headers)
-                print('User Agent:', page.evaluate("() => navigator.userAgent"))
+                userAgent = await page.evaluate("() => navigator.userAgent")
+                print('User Agent:', userAgent)
                 content = await page.content()
                 context = page.context
                 print('content', content)
