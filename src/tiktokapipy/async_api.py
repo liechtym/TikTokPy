3cf1272f1c8648ce4b59e7f89dedcbafa7befafd
Matthew Liechty <matthew@liechty.org>
2023-02-14 10:47:39 -0500

    Update async_api.py

--- src/tiktokapipy/async_api.py
+++ src/tiktokapipy/async_api.py
@@ -181,6 +181,7 @@ class AsyncTikTokAPI(TikTokAPI):
                 print('User Agent:', userAgent)
                 content = await page.content()
                 context = page.context
+                print('Request Headers:', res.request.all_headers())
                 print('content', content[0:100])
                 print('context', context)
                 await page.wait_for_selector("#SIGI_STATE", state="attached")
